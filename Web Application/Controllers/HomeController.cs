d844a9aba54f91acb7e53c34ed105f322091ba45
99khalid <khalidhassan912999@gmail.com>
2023-10-17 11:35:16 +0300

    task day 2

---
+++ Web Application/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web_Application.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+        public IActionResult PendingPage()
+        {
+            return View();
+        }
+    }
+}
---
+++ Web Application/Controllers/ProductController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Models;
+namespace Web_Application.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
